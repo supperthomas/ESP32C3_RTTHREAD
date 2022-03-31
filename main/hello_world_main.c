c71d26bd4212f00e991c305f4af1faa82d03f817
supperthomas <78900636@qq.com>
2022-03-31 22:28:36 +0800

    add rtthread

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -1,43 +1,31 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
-#include "sdkconfig.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_spi_flash.h"
-
+#include "driver/gpio.h"
+#define BLINK_GPIO 8
+#define LOOP_COUNT 50000000
+#include "rtthread.h"
 void app_main(void)
 {
     printf("Hello world!\n");
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), WiFi%s%s, ",
-            CONFIG_IDF_TARGET,
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-
-    printf("silicon revision %d, ", chip_info.revision);
-
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    uint32_t count = 0;
+    gpio_reset_pin(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    while(1) {
+        /* Blink off (output low) */
+        gpio_set_level(BLINK_GPIO, 0);
+        count= LOOP_COUNT;
+        while(count)
+        {
+            count--;
+        }
+        /* Blink on (output high) */
+        printf("Turning on the LED\n");
+        gpio_set_level(BLINK_GPIO, 1);
+        count= LOOP_COUNT;
+        while(count)
+        {
+            count--;
+        }
     }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
 }
